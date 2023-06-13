f6050607681af60eb8f65e38d1eea75c2f008aa0
José Antonio Del Rey Martínez <72363448+Janto7@users.noreply.github.com>
2023-06-13 14:14:33 +0200

    Creada pagina estatica not-found

--- musik-leven/src/App.js
+++ musik-leven/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import Contacto from './publica/paginas/estaticas/contacto/Contacto';
 import Home from './publica/paginas/estaticas/home/Home';
 import SobreNosotros from './publica/paginas/estaticas/sobre-nosotros/SobreNosotros';
+import NotFound from './publica/paginas/estaticas/not-found/NotFound';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/contacto" element={<Contacto />} />
         <Route path="/sobre-nosotros" element={<SobreNosotros />}/>
-        <Route path="*" element={<Navigate to="/" />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
---
+++ musik-leven/src/publica/paginas/estaticas/not-found/NotFound.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class NotFound extends Component {
+  render() {
+    return (
+      <>
+        <h1>404</h1>
+        <p>Not found</p>
+      </>
+    );
+  }
+}
+
+export default NotFound;
