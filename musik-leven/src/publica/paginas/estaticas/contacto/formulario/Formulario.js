637b03cace7b073b64b18fb873f3aed1698c28a1
Rubén M <rubenmagjat@gmail.com>
2023-06-15 14:53:15 +0200

    formulario contacto con validador created

--- musik-leven/src/publica/paginas/estaticas/contacto/formulario/Formulario.js
+++ musik-leven/src/publica/paginas/estaticas/contacto/formulario/Formulario.js
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import Validador from "./validador/Validador";
 
 export default class Formulario extends Component {
 
@@ -6,50 +7,124 @@ export default class Formulario extends Component {
 
         super(props);
 
-        this.state = { nombre: "", email: "", asunto: "", mensaje: "" }
+        this.state = {
+            values: {
+                nombre: "", email: "", asunto: "", mensaje: ""
+            },
+            validations: {
+                nombre: [], email: [], asunto: [], mensaje: []
+            }
+        }
 
     }
 
     handleChange = (e) => {
 
         const { name, value } = e.target;
-        this.setState({ [name]: value })
+        this.setState(
+            {
+
+                values: {
+                    ...this.state.values,
+                    [name]: value
+                }
+            }
+        )
 
     }
 
     handleSubmit = (e) => {
 
         e.preventDefault();
+        const isValid = this.validateAll();
 
-        const datos = this.state;
+        if (!isValid) {
 
-        if (datos.nombre != null && datos.nombre !== "" && datos.emial !== null && datos.email !== "" && datos.asunto !== null && datos.asunto !== "" && datos.mensaje !== null && datos.mensaje !== "") { //compruebo que los datos que llegan no son vacíos
-
-            if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(datos.email)) { //si no son vacíos, compruebo que el usuario no quite el tipo email y meta email incorrecto
-
-                const values = JSON.stringify(this.state);
-                console.log(values);
-                alert("Su mensaje ha sido enviado al admin");
-
-            } else {
-
-                alert("Formato de email erróneo");
-
-            }
-
-
-        } else {
-
-            alert("Aún quedan campos por rellenar");
+            return false;
 
         }
 
+        alert("Se ha enviado un correo al dmin con tu mensaje");
+
+
+    }
+
+    validateAll = () => {
+
+        const { nombre, email, asunto, mensaje } = this.state.values;
+
+        const validations = { nombre: "", email: "", asunto: "", mensaje: "" };
+
+        validations.nombre = this.validateNombre(nombre);
+        validations.email = this.validateEmail(email);
+        validations.asunto = this.validateAsunto(asunto);
+        validations.mensaje = this.validateMensaje(mensaje);
+
+        const mensajesValidacion = Object.values(validations).filter(mensaje => mensaje.length);
+
+        const isValid = !mensajesValidacion.length;
+
+        if (!isValid) {
+
+            this.setState({ validations });
+
+        }
+
+
+
+        return isValid;
+
+    }
+
+    validateNombre = (name) => {
+
+        const validadorNombre = new Validador(name);
+
+        return validadorNombre
+            .isNotEmpty("Nombre requerido")
+            .isLength(50, "La longitud de nombre no es correcta (max 50)")
+            .result
+
+    }
+
+    validateEmail = (name) => {
+
+        const validadorEmail = new Validador(name);
+
+        return validadorEmail
+            .isNotEmpty("Email requerido")
+            .isEmail("Formato email incorrecto")
+            .result
+
+    }
+
+    validateAsunto = (name) => {
+
+        const validadorAsunto = new Validador(name);
+
+        return validadorAsunto
+            .isNotEmpty("Asunto requerido")
+            .isLength(50, "La longitud de asunto no es correcta (max 100)")
+            .result
+
+    }
+
+    validateMensaje = (name) => {
+
+        const validadorMensaje = new Validador(name);
+
+        return validadorMensaje
+            .isNotEmpty("Mensaje requerio")
+            .isLength(200, "La longitud de mensaje no es correcta (max 200)")
+            .result
 
     }
 
     render() {
 
         const { nombre, email, asunto, mensaje } = this.state;
+
+        const { nombre: nameVal, email: emailVal, asunto: asuntoVal, mensaje: mensajeVal } = this.state.validations
 
         return (
 
@@ -65,12 +140,20 @@ export default class Formulario extends Component {
                             <input type='text' name="nombre" value={nombre} onChange={this.handleChange}></input>
                         </p>
 
+                        <div>
+                            {nameVal}
+                        </div>
+
                         <p>
                             <label>Email:</label>
                         </p>
                         <p>
                             <input type='email' name="email" value={email} onChange={this.handleChange}></input>
                         </p>
+
+                        <div>
+                            {emailVal}
+                        </div>
 
                         <p>
                             <label>Asunto:</label>
@@ -79,12 +162,20 @@ export default class Formulario extends Component {
                             <input type='text' name="asunto" value={asunto} onChange={this.handleChange}></input>
                         </p>
 
+                        <div>
+                            {asuntoVal}
+                        </div>
+
                         <p>
                             <label>Mensaje:</label>
                         </p>
                         <p>
                             <textarea name="mensaje" value={mensaje} onChange={this.handleChange} ></textarea>
                         </p>
+
+                        <div>
+                            {mensajeVal}
+                        </div>
 
                         <p>
                             <input type='submit' value="Enviar" />
